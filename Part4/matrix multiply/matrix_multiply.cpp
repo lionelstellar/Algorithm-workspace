7e3dd88b080299b98dc8455e7ac6580d2b359a6d
lionelstellar <1023612737@qq.com>
2019-06-17 03:02:17 +0800

    6-17

--- Part4/matrix multiply/matrix_multiply.cpp
+++ Part4/matrix multiply/matrix_multiply.cpp
@@ -2,15 +2,43 @@
 #include<vector>
 using namespace std;
 /**
- * method 1:
  * @brief matrix chain order problem (bottom-up method)
  * @param p     the colume of A1~n, p0 is the row of A1
  * @param m     m[i,j] is the optimization of matrix multiply of Ai to Aj
  * @param s     s[i,j] is the break point of matrix chain Ai...j in the optimization
+ * equation:    m[i,j] = min(m[i,k] + m[k+1,j] + Ai-1AkAj) (i<k<j)
+ *              m[i,i] = 0
  */
-void matrix_chain_order(vector<int> p, int** &m, int** &s)
+void matrix_chain_order(vector<int> p, vector< vector<int> > &m, vector< vector<int> > &s)
 {
-
+    int temp;
+    // i表示起始矩阵Ai;
+    // gap表示Ai到Aj的距离, gap = j - i. 当gap值越大时, 可以用到小gap的子问题解.
+    for(int gap=1; gap <= p.size()-2; gap++)
+        for(int i = 1; i <= p.size() - gap - 1; i++){
+            m[i][i + gap] = INT_MAX;
+            for(int k = i; k < i + gap; k++){
+                temp = m[i][k] + m[k+1][i+gap] + p[i-1]*p[k]*p[i + gap];
+                if(m[i][i + gap] > temp){
+                    m[i][i + gap] = temp;
+                    s[i][i + gap] = k;
+                } 
+            }
+        }
+}
+/**
+ * @brief print the optimal partern to caculate the multiplication of matrix chain
+ * @param s     s[i,j] is the break point of matrix chain Ai...j in the optimization
+ */
+void print_optimal_partern(vector< vector<int> > s, int i, int j){
+    if(i == j)
+        cout << "A" << i;
+    else{
+        cout << "(";
+        print_optimal_partern(s,i,s[i][j]);
+        print_optimal_partern(s,s[i][j] + 1,j);
+        cout << ")";
+    }
 }
 
 int main()
@@ -19,9 +47,29 @@ int main()
     int len = sizeof(arr)/sizeof(int);
     vector<int> v(arr, arr + len);
 
-    int **m;
-    m = (int **)malloc(len*len*sizeof(int));
+    // 初始化二维向量m和s, 第0行和第0列无意义
+    vector< vector<int> > m(len+1, vector<int>(len+1, 0));
+    vector< vector<int> > s(len+1, vector<int>(len+1, 0));
+    
+    matrix_chain_order(v,m,s);
 
+    // 打印m表
+    cout << "m表：" << endl;
+    for(int i = 1; i < len; i++){
+        for(int j = i; j < len; j++)
+            cout <<  m[i][j] << ",";
+        cout << endl;
+    }
 
-
+    // 打印s表
+    cout << "s表：" << endl;
+    for(int i = 1; i < len; i++){
+        for(int j = i + 1; j < len; j++)
+            cout <<  s[i][j] << ",";
+        cout << endl;
+    }
+    // 打印A1乘到A6的最优加括号方法   
+    print_optimal_partern(s,1,6);
+    
+            
 }
--- debug.cpp
+++ debug.cpp
@@ -1,96 +1,78 @@
-/**
- * @brief cut rod problem
- * price table
- * length i  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  10  |
- * price  p  |  1  |  5  |  8  |  9  |  10 |  17 |  17 |  20 |  24 |  30  |
- * 
- * How can we make most of the rod of length L, return the most we can sell.
- * 
- */
 #include<iostream>
 #include<vector>
 using namespace std;
-static int p[11] = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
-static int max_len = 10;
-
 /**
- * @brief cut rod problem (top-down method)
- * @param n  the length of rod we have
+ * @brief matrix chain order problem (bottom-up method)
+ * @param p     the colume of A1~n, p0 is the row of A1
+ * @param m     m[i,j] is the optimization of matrix multiply of Ai to Aj
+ * @param s     s[i,j] is the break point of matrix chain Ai...j in the optimization
+ * equation:    m[i,j] = min(m[i,k] + m[k+1,j] + Ai-1AkAj) (i<k<j)
+ *              m[i,i] = 0
  */
-int cut_rod(int n)
+void matrix_chain_order(vector<int> p, vector< vector<int> > &m, vector< vector<int> > &s)
 {
-    if(n == 0)
-        return 0;
-    int j = 0;
-    int q = INT_MIN;
-    for(j = 1; j <= n; j++){
-        if(j <= max_len)
-            q = max(q, p[j] + cut_rod(n-j));
-        
-    }
-    return q;
-}
-
-/**
- * @brief cut rod problem (top-down with memorization)
- * @param n  the length of rod we have
-*/
-int memoized_cut_rod_aux(int n, vector<int> &v)
-{
-    if(v[n] >= 0)
-        return v[n];
-    int q;
-    if(n == 0)
-        q = 0;
-    else{
-        q = INT_MIN;
-        for(int i = 1; i <= n; i++){
-            if(i <= max_len)
-                q = max(q, p[i] + memoized_cut_rod_aux(n-i,v));
-        }       
-    }
-    v[n] = q;
-    return q;
-}
-
-int memorized_cut_rod(int n)
-{
-    vector<int> v(n+1,INT_MIN);
-    
-    
-    return memoized_cut_rod_aux(n,v);
-    
-}
- 
-
-
-/**
- * @brief cut rod problem (bottom_up method)
- * @param n  the length of rod we have
- */
-int bottom_up_cut_rod(int n)
-{
-    int i,j = 0;
-    int q;
-    vector<int> v(n+1, 0);
-    
-    for(j = 1; j <= n; j++){
-        q = INT_MIN;
-        for(i = 0; i <= j; i++){
-            if(i <= max_len)
-                q = max(q, p[i] + v[j-i]);
+    int temp;
+    // i表示起始矩阵Ai;
+    // gap表示Ai到Aj的距离, gap = j - i. 当gap值越大时, 可以用到小gap的子问题解.
+    for(int gap=1; gap <= p.size()-2; gap++)
+        for(int i = 1; i <= p.size() - gap - 1; i++){
+            m[i][i + gap] = INT_MAX;
+            for(int k = i; k < i + gap; k++){
+                temp = m[i][k] + m[k+1][i+gap] + p[i-1]*p[k]*p[i + gap];
+                if(m[i][i + gap] > temp){
+                    m[i][i + gap] = temp;
+                    s[i][i + gap] = k;
+                } 
+            }
         }
-        v[j] = q;
-        
+}
+/**
+ * @brief print the optimal partern to caculate the multiplication of matrix chain
+ * @param s     s[i,j] is the break point of matrix chain Ai...j in the optimization
+ */
+void print_optimal_partern(vector< vector<int> > s, int i, int j){
+    if(i == j)
+        cout << "A" << i;
+    else{
+        cout << "(";
+        print_optimal_partern(s,i,s[i][j]);
+        print_optimal_partern(s,s[i][j] + 1,j);
+        cout << ")";
     }
-    return v[n];
 }
 
 int main()
 {
-    cout << "自顶向下法切长度为10：" << cut_rod(10) << endl;
+    int arr[] = {30, 35, 15, 5, 10, 20, 25};
+    int len = sizeof(arr)/sizeof(int);
+    vector<int> v(arr, arr + len);
 
-    cout << "带备忘录自顶向下法切长度为8：" << memorized_cut_rod(8) << endl;
-    cout << "自底向上法切长度为9：" << bottom_up_cut_rod(9) << endl;
+    // 初始化二维向量m和s, 第0行和第0列无意义
+    vector< vector<int> > m(len+1, vector<int>(len+1, 0));
+    vector< vector<int> > s(len+1, vector<int>(len+1, 0));
+    
+    matrix_chain_order(v,m,s);
+
+    // 打印m表
+    cout << "m表：" << endl;
+    for(int i = 1; i < len; i++){
+        for(int j = i; j < len; j++)
+            cout <<  m[i][j] << ",";
+        cout << endl;
+    }
+
+    // 打印s表
+    cout << "s表：" << endl;
+    for(int i = 1; i < len; i++){
+        for(int j = i + 1; j < len; j++)
+            cout <<  s[i][j] << ",";
+        cout << endl;
+    }
+    // 打印A1乘到A6的最优加括号方法   
+    print_optimal_partern(s,1,6);
+    
+            
+
+
 
 }
