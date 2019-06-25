1cf5b6a2ce735dd2e6712d55b355f57d7cfb0fa6
lionelstellar <1023612737@qq.com>
2019-06-25 22:32:27 +0800

    6-25

--- Part4/Greedy Algorithm/huffman.cpp
+++ Part4/Greedy Algorithm/huffman.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
+#include<map>
 using namespace std;
+
+class HuffmanTreeNode{
+    public:
+        char key;
+        double weight;
+        HuffmanTreeNode *parent;
+        HuffmanTreeNode *left;
+        HuffmanTreeNode *right;
+        HuffmanTreeNode(char k, double w, HuffmanTreeNode *p, HuffmanTreeNode *l, HuffmanTreeNode *r):
+        key(k), weight(w), parent(p), left(l), right(r){}
+};
+
 int main()
 {
+    map<char, int> dict;
+    char ch[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+    int freq[] = {45, 13, 12, 16, 9, 5};
+    
     
 }
