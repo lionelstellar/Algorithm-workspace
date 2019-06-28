b97f0d81146c68268562e6c56638972bcb489a93
lionelstellar <1023612737@qq.com>
2019-06-28 23:42:51 +0800

    6-28

--- Part4/Greedy Algorithm/huffman.cpp
+++ Part4/Greedy Algorithm/huffman.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<map>
+#include<vector>
 using namespace std;
 
 class HuffmanTreeNode{
@@ -12,12 +12,29 @@ class HuffmanTreeNode{
         HuffmanTreeNode(char k, double w, HuffmanTreeNode *p, HuffmanTreeNode *l, HuffmanTreeNode *r):
         key(k), weight(w), parent(p), left(l), right(r){}
 };
+// 找到最小的值的下标
+int extract_min(vector<int> v)
+{
+    int min = INT_MAX;
+    int flag = 0;
+    for(int i = 0; i < v.size(); i++){
+        if(v[i] < min){
+            flag = i;
+            min = v[i];
+        }
+    }
+
+    return flag;
+}
 
 int main()
 {
     
     char ch[] = {'a', 'b', 'c', 'd', 'e', 'f'};
     int freq[] = {45, 13, 12, 16, 9, 5};
+    vector<int> freq_vec(ch, ch + sizeof(ch) / sizeof(int));
+
+    
     
     
     
